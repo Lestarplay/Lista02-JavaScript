5ccfeb72f340fbc270891d7b1a1b5be4cbe0075f
Breno B.franco <162745949+Lestarplay@users.noreply.github.com>
2025-10-07 21:05:58 -0300

    Add files via upload

---
+++ js/script10.js
@@ -0,0 +1,13 @@
+let numero01 = parseInt(prompt("Digite um número: "))
+let numero02 = parseInt(prompt("Digite um outro número: "))
+let numero03 = parseInt(prompt("Digite um outro número: "))
+
+if(numero01 > numero02 > numero03){
+    alert(numero01 + numero02 + numero03)
+}if(numero02 > numero01 > numero03){
+   alert(numero02 + numero01 + numero03)
+}if(numero03 > numero02 > numero01){
+   alert(numero03 + numero02 + numero01)
+}
+
+alert(numero01 + " e " + numero02 + " e " + numero03)
---
+++ js/script11.js
@@ -0,0 +1,7 @@
+let faltas = parseInt(prompt("Digite o número de faltas do aluno: "))
+
+if(faltas <= 15 ){
+  alert("O aluno não foi reprovado! ")
+}else{
+    alert("O aluno foi reprovado por faltas! ")
+}
